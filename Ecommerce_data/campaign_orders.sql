b799e1f2550c3fe8b5252f349a50d632e872bf8d
Krishnasurya Murali <krishnamurali09@outlook.com>
2025-10-04 20:42:04 -0600

    Add campaign_orders table creation script to aggregate order data

---
+++ Ecommerce_data/campaign_orders.sql
@@ -0,0 +1,10 @@
+CREATE TABLE public.campaign_orders AS
+ SELECT
+  o.order_id,
+  o.order_date::date                   AS dt,
+  o.campaign_id,
+  o.customer_id,
+  o.revenue,
+  o.gross_profit,
+  (o.order_date = o.first_order_date  AND o.campaign_id IS NOT NULL) AS is_new_customer
+FROM public.orders o
